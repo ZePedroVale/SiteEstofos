2eb282ab25ed04dcd7830e1a98d655cd337c4acd
Jose Vale <jpbvale09@gmail.com>
2024-01-16 14:13:48 +0000

    novas rotas (sobre,contactos)

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('home.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contactos')
+def contactos():
+    return render_template('contactos.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
